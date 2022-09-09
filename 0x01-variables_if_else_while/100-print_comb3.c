943087ff057006ecd7f286533cf72f3419652899
girlbehindthescreen <igwuokuchristabel@gmail.com>
2022-09-09 05:04:55 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,14 @@
-#include<stdio.h>                                                                                                      
-                                                                                                                       
-/**                                                                                                                    
- * main - Entry point                                                                                                  
- *                                                                                                                  
- * Return: Always 0 (Success)                                                                                          
- */                                                                                                                    
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int digit1, digit2;
+
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
