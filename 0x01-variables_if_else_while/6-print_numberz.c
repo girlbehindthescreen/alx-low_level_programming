801d8b19885d7aab5d774dd4203bfcf05664840b
girlbehindthescreen <igwuokuchristabel@gmail.com>
2022-09-09 06:30:40 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,18 @@
-#include<stdio.h>                                                                                                      
-                                                                                                                       
-/**                                                                                                                    
- * main - Entry point                                                                                                  
- *                                                                                                                     
- * Return: Always 0 (Success)                                                                                          
- */                                                                                                                    
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
+
 	for (i = 48; i <= 57; i++)
 	{
 		putchar (i);
 	}
-	purchar ('\n');
+	putchar('\n');
 	return (0);
 }
