f6df850b01570654ff024bf14637560320b5c711
girlbehindthescreen <igwuokuchristabel@gmail.com>
2022-09-09 04:49:48 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success) 
- */ 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alphabet;
-	
+
 	for (alphabet = 122 ; alphabet >= 97; alphabet--)
 	{
 		putchar(alphabet);
