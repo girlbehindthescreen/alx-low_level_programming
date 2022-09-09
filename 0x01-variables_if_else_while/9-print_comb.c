30420f7c16e715535951169720abf18cfb975871
girlbehindthescreen <igwuokuchristabel@gmail.com>
2022-09-09 04:57:24 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,14 @@
-#include<stdio.h>                                                                                                      
-                                                                                                                       
-/**                                                                                                                    
- * main - Entry point                                                                                                  
- *                                                                                                                  
- * Return: Always 0 (Success)                                                                                          
- */                                                                                                                    
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int number;
+
 	for (number = 48; number <= 57; number++)
 	{
 		putchar(number);
