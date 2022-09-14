47a83f01ba2f923865ae9bb6fe4d3934426263d2
girlbehindthescreen <igwuokuchristabel@gmail.com>
2022-09-14 03:58:03 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * prints_remaining_days - takes a date and prints how many days are
+ * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
- * @month: day of month
+ * @month: month in number format
+ * @day: day of month
  * @year: year
- * Return; void
+ * Return: void
  */
 void print_remaining_days(int month, int day, int year)
 {
