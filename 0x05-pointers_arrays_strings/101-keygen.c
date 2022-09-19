a9419f74df55b1f53a2972dff6176f159724647a
girlbehindthescreen <igwuokuchristabel@gmail.com>
2022-09-19 15:24:40 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,17 +11,44 @@
 
 int main(void)
 {
-	char c;
-	int x;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
-	while (x <= 2645)
-	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
-	}
-	putchar(2772 - x);
 
-	return (0);
-}
+	while (sun < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = o; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+			for (index = 0; password[index]; index++)
+			{
+				if (password[index] >= (33 + diff_half2))
+				{
+					password[index] -= diff_half2;
+					break;
+				}
+			}
+		}
+		printf("%s", password);
+
+		return (0);
+	}
+
