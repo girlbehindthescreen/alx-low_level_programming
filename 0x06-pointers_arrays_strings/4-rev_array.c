1fb5062ec28e55c9598b32228444c2de5c42c37b
girlbehindthescreen <igwuokuchristabel@gmail.com>
2022-09-21 12:25:57 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,30 @@
 #include "main.h"
 
 /**
- * reverse_array -> reversing an array
- * @a: array a
- * @n: an element of an array
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-void reverse_array(int *a, int n)
+char *cap_string(char *s)
 {
-	int *p, i, aux, k;
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < i / 2; k++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == separators[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
 	}
+	return (s);
 }
