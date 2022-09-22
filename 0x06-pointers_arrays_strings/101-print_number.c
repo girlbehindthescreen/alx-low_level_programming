bd538aa906840f55a277e315a6ee7637097e45db
girlbehindthescreen <igwuokuchristabel@gmail.com>
2022-09-22 01:33:18 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,29 +7,18 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
+		_putchar('-');
+		n *= -1;
 	}
 
-	d = m;
-	count = 1;
+	x = n;
 
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-	
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
