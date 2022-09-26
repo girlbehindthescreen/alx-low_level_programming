cd235650cd21671835f02ce4212b93ad2c644cab
Igwuoku Christabel <71432850+girlbehindthescreen@users.noreply.github.com>
2022-09-26 06:07:35 -0500

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
-* _strchr -> string character
-* @s: string given
-* @c: another char
-* Return: a string
-*/
+ * _strchr - locates a character in a string
+ *
+ * @s: string to search
+ * @c: character to search for
+ *
+ * Return: pointer to first occurence of c in s
+ */
 char *_strchr(char *s, char c)
 {
-		int a;
-
-		while (1)
-		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
-		}
+	while (*s != 0)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (0);
 }
