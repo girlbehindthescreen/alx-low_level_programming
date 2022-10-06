540d4ba595ac7629d030f7ec8b6c4efe36d83e4c
Igwuoku Christabel <71432850+girlbehindthescreen@users.noreply.github.com>
2022-10-06 13:29:45 -0500

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -125,22 +125,21 @@ int main(int argc, char *argv[])
 	if (a == NULL)
 	{
 		for (ti = 0; e[ti}; ti++)
-		         _putchar(e[ti]);
-		exit(98);
-	}
-	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
-	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln -2) - i);
-		if (t == NULL)
-		{
-			for (ti = 0; e[ti]; ti++)
-				 _putchar(e[ti]);
-			free(a);
-			exit(98);
-		}
+			_putchar(e[ti]);
+	        exit(98);
 	 }
-	 _print(a, ln - 1);
-	 return (0);
+		init(a, ln - 1);
+			    for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+			       {
+				       t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln -2) - i);
+				       if (t == NULL)
+				       {
+					       for (ti = 0; e[ti]; ti++)
+						       _putchar(e[ti]);
+					       free(a);
+					       exit(98);
+				       }
+			       }
+			       _print(a, ln - 1);
+			       return (0);
 }
-
